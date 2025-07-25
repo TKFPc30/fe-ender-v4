73a2a87fc52edb50a2a8bee1e4ae2d21c77ff034
TKFPc30 <aisyrapid@gmail.com>
2025-07-26 01:07:47 +0700

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,12 @@
+--[[
+	WARNING: Heads up! This script has not been verified by ScriptBlox. Use at your own risk!
+]]
+	game:GetService("TextChatService").TextChannels.RBXGeneral:SendAsync("-gh 48474313 63690008 62234425 62724852 48474294 451220849")
+
+	wait(1
+
+	game:GetService("TextChatService").TextChannels.RBXGeneral:SendAsync("-net")
+
+	wait(1)
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/TKFPc30/fe-ender/main/main.lua'))()
